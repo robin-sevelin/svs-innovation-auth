0a471c3a20b14d997cfedebce753da6051031789
robin-sevelin <robinsevelin@hotmail.com>
2025-01-28 08:54:35 +0100

    install auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  const title = 'Hello auth';
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1 className='bold text-2xl'>{title.toUpperCase()}</h1>
+      <h1 className='bold text-2xl'>Hello auth</h1>
     </div>
   );
 }
