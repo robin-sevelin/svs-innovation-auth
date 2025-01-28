a643366c4b35e8970fa6ad8223c057a9fa7a2c73
robin-sevelin <robinsevelin@hotmail.com>
2025-01-28 11:47:31 +0100

    log in happy flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
         <SessionProvider>
+          <Header />
           <main className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
             {children}
           </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,36 @@
+'use client';
+
 import React from 'react';
+import { signIn, useSession, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 const Button = () => {
+  const { data: session, status } = useSession();
   return (
     <div>
-      <button>My button</button>
+      {status === 'authenticated' ? (
+        <button
+          onClick={() => {
+            signOut();
+          }}
+        >
+          <Image
+            src={session?.user?.image || '/public/placeholder.jpg'}
+            alt='profile picture'
+            width={50}
+            height={50}
+          />
+          Sign out
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            signIn('github');
+          }}
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 };
